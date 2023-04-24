bb66619aa8ce9291330d5611661c0b18e18d3d25
keulreobeu <112425846+keulreobeu@users.noreply.github.com>
2023-04-24 17:55:38 +0900

    Delete chapter11 directory

--- chapter11/roboadvisor/analysis/statics.py
+++
@@ -1,2 +0,0 @@
-def statics_test():
-    print('statics')
--- chapter11/using_thuld.py
+++
@@ -1,8 +0,0 @@
-# import matplotlib as npl  잘안씀
-import matplotlib.pyplot as plt
-
-plt.plot([1,2,3,4])
-plt.ylabel('some')
-
-plt.show()
-
