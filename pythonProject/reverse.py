dcbfa300a269e4b3950f5027e31126cca3da5c3f
keulreobeu <112425846+keulreobeu@users.noreply.github.com>
2023-04-24 17:56:47 +0900

    Delete pythonProject directory

--- pythonProject/reverse.py
+++
@@ -1,7 +0,0 @@
-my_sty = input('문자열을 입력하십시오: ')
-
-sty_cnt = len(my_sty)
-sty = ''
-for i in range(sty_cnt-1, -1 , -1):
-    sty += my_sty[i]
-print(sty)
--- pythonProject/while.py
+++
@@ -1,5 +0,0 @@
-i = 0
-count = int(input('반복횟수를 입력하세요. :'))
-while i < count:
-    print('hello')
-    i+=1
